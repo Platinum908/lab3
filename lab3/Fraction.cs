ef5f11e2a93b3b3f1874cac94c328400e0042949
Platinum908 <estnuntius@gmail.com>
2024-06-28 08:01:01 +0200

    Добавьте файлы проекта.

---
+++ lab3/Fraction.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace lab3
+{
+    public class Fraction
+    {
+        int whole { get; set; }
+        int numerator{ get; set; }
+        int denominator { get; set; }
+
+        public Fraction(int whole, int numerator, int denominator)
+        {
+            this.whole = whole;
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            return new Fraction(a.whole + b.whole, a.numerator + b.numerator, a.denominator + b.denominator);
+        }
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            return new Fraction(a.whole - b.whole, a.numerator - b.numerator, a.denominator - b.denominator);
+        }
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            return new Fraction(a.whole * b.whole, a.numerator * b.numerator, a.denominator * a.denominator);
+        }
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            return new Fraction(a.whole / b.whole, a.numerator / b.numerator, a.denominator / a.denominator);
+        }
+        public static bool operator ==(Fraction a, Fraction b)
+        {
+            if (a.whole == b.whole && a.numerator == b.numerator && a.denominator ==  a.denominator)
+            { return true; }
+            return false;
+        }
+        public static bool operator !=(Fraction a, Fraction b)
+        {
+            if (a.whole != b.whole || a.numerator != b.numerator || a.denominator != a.denominator)
+            { return true; }
+            return false; ;
+        }
+        public override string ToString()
+        {
+            return whole + "  " + numerator + "/" + denominator;
+        }
+    }
+}
---
+++ lab3/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void plus_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Fraction fr = new Fraction(int.Parse(whol.Text), int.Parse(numerator1.Text), int.Parse(den1.Text));
+                Fraction fr2 = new Fraction(int.Parse(whol2.Text), int.Parse(numerator2.Text), int.Parse(den2.Text));
+                Fraction res = fr + fr2;
+                wholefull.Text = res.ToString();
+            }
+            catch {
+                MessageBox.Show("Вводите числа");
+            }
+        }
+
+        private void minus_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Fraction fr = new Fraction(int.Parse(whol.Text), int.Parse(numerator1.Text), int.Parse(den1.Text));
+                Fraction fr2 = new Fraction(int.Parse(whol2.Text), int.Parse(numerator2.Text), int.Parse(den2.Text));
+                Fraction res = fr - fr2;
+                wholefull.Text = res.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Вводите числа");
+            }
+
+        }
+
+        private void multi_Click(object sender, RoutedEventArgs e)
+        {
+            try { 
+            Fraction fr = new Fraction(int.Parse(whol.Text), int.Parse(numerator1.Text), int.Parse(den1.Text));
+            Fraction fr2 = new Fraction(int.Parse(whol2.Text), int.Parse(numerator2.Text), int.Parse(den2.Text));
+            Fraction res = fr * fr2;
+            wholefull.Text = res.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Вводите числа");
+            }
+        }
+
+        private void comp_Click(object sender, RoutedEventArgs e)
+        {
+            try {
+            Fraction fr = new Fraction(int.Parse(whol.Text), int.Parse(numerator1.Text), int.Parse(den1.Text));
+            Fraction fr2 = new Fraction(int.Parse(whol2.Text), int.Parse(numerator2.Text), int.Parse(den2.Text));
+            bool v = fr == fr2;
+            wholefull.Text = v.ToString();
+            }
+             catch
+                 {
+                MessageBox.Show("Вводите числа");
+                 }
+}   
+    }
+}
